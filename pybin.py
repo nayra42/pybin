1ade4befc706237e327b06f5e2969352c4272cee
nayra42 <ryan.w.coryell@gmail.com>
2019-10-01 11:31:05 -0600

    Create pybin.py
    Initial commit of working scraper, this requires a pro license to pastebin.

---
+++ pybin.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python3
+
+"""
+written by Ryan Coryell
+designed to scrape pastebin for interesting stuff
+"""
+import re 
+import requests
+import time 
+import json
+import urllib
+import os
+import smtplib
+import logging
+
+##LOGING CONFIG BLOCK
+#make a filelogger
+filelogger = logging.getLogger(__name__)
+#set logging level - debugging
+#filelogger.setLevel(logging.DEBUG)
+#set logging level - info
+filelogger.setLevel(logging.INFO)
+#setup handler
+filelogger_handler = logging.FileHandler("/opt/pybin/python/pybin.log")
+#format logs
+filelogger_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S %Z")
+# Apply the formatter to the handler
+filelogger_handler.setFormatter(filelogger_format)
+# Apply the handler to the logger
+filelogger.addHandler(filelogger_handler)
+
+while True:
+    try:
+
+#def latest_pastes():
+#this is the url to append the key to to get the raw paste
+        rawPasteURL = 'https://scrape.pastebin.com/api_scrape_item.php?i='
+#this is the url to grab keys, note a limit can be set with ?limit=x 
+        binInput = requests.get('https://scrape.pastebin.com/api_scraping.php?limit=100')
+        filelogger.debug(binInput)
+        filelogger.debug(dir(binInput))
+        filelogger.debug(binInput.text)
+        #input()
+        binInput = binInput.json()
+       
+        keyList = []
+        scrapedList = []
+        scrapedKeys = []
+        pageDict = {}
+
+
+
+#pull keys from pastebin and put in list
+        for i in binInput:
+            #print(i)
+            keyList.append(i['key'])
+        #print(keyList)#test print
+
+#read historical list from file into memory
+        #with open(os.path.join('/opt/pybin/python/files/keys/',"scrapedKeys"+".txt"), 'a+') as f:
+        try:
+            with open('/opt/pybin/python/files/keys/scrapedKeys.txt') as f:
+                scrapedKeys = f.readlines()
+            filelogger.debug(scrapedKeys)
+            #input
+        except IOError:
+            with open('/opt/pybin/python/files/keys/scrapedKeys.txt', 'w+')  as f:     
+                pass
+
+#remove line break char
+        for index, value in enumerate(scrapedKeys):
+            scrapedKeys[index] = value.rstrip("\n")
+        #print(scrapedKeys)#testprint
+
+#check to see if keyList keys already exist in scrapedKeys, if not skip and write to scrapedList      
+        for index, value in enumerate(keyList):
+            if value in scrapedKeys:
+                print(value)
+                pass
+            else :
+                scrapedList.append(keyList[index])
+            
+       #print(scrapedList)#print keys to scrape
+
+#Append scrapedKeys File
+        with open(os.path.join('/opt/pybin/python/files/keys/',"scrapedKeys"+".txt"), 'a+') as f:
+            for item in scrapedList:
+                f.write("%s\n" % item)
+
+#write to scraped list
+        with open(os.path.join('/opt/pybin/python/files/keys/',"scrapedList"+".txt"), 'w+') as f:
+            for item in scrapedList:
+                 f.write("%s\n" % item)
+
+#get pastes
+        for i in scrapedList:
+            reqResult = requests.get(rawPasteURL+i)
+        #print(reqResult.text)#test print of raw pastes
+            pageDict[i] = reqResult.text
+   
+        #for k,v in pageDict.items():
+            #print(k,v)#test print of dict with pastes
+ 
+        #print("             ")#spacer
+        refilter = re.compile(r'(?is)password')
+
+        for x,y in pageDict.items():
+            result = refilter.findall(y) 
+    #print(result)
+    #print(type(result))
+    #print(len(result))
+            if len(result) >= 1:
+                sep = "*" * 30
+                with open(os.path.join('/opt/pybin/python/files/pastes/',"pasteList"+".txt"), 'a+') as f:
+                    f.write("\n{0}\n{1}\n{2}".format(x,result[0],sep))
+    
+        time.sleep(1)
+    
+    except Exception as e:
+        filelogger.exception("EXCEPTIONSTART", exc_info=True)
+        pass
