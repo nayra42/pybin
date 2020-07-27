afaa03e08c201e5a0d3c9a960b00fe7b4c2b28bc
Ryan Coryell <ryan.w.coryell@gmail.com>
2020-07-27 13:41:59 -0600

    Update pybin.py
    cleaned up the new line due to loging issues when reading the flat file with a SEIM.

--- pybin.py
+++ pybin.py
@@ -2,11 +2,12 @@
 
 """
 written by Ryan Coryell
-designed to scrape pastebin for interesting stuff
+designed to scrape pastebin for interesting stuff use regex to define what you want to look for run this as a service in system d for constant run in the background.  
+You will need a pastebin pro account to use this script.
 """
-import re 
+import re
 import requests
-import time 
+import time
 import json
 import urllib
 import os
@@ -36,14 +37,14 @@ while True:
 #def latest_pastes():
 #this is the url to append the key to to get the raw paste
         rawPasteURL = 'https://scrape.pastebin.com/api_scrape_item.php?i='
-#this is the url to grab keys, note a limit can be set with ?limit=x 
+#this is the url to grab keys, note a limit can be set with ?limit=x
         binInput = requests.get('https://scrape.pastebin.com/api_scraping.php?limit=100')
         filelogger.debug(binInput)
         filelogger.debug(dir(binInput))
         filelogger.debug(binInput.text)
         #input()
         binInput = binInput.json()
-       
+
         keyList = []
         scrapedList = []
         scrapedKeys = []
@@ -65,7 +66,7 @@ while True:
             filelogger.debug(scrapedKeys)
             #input
         except IOError:
-            with open('/opt/pybin/python/pybin/files/keys/scrapedKeys.txt', 'w+')  as f:     
+            with open('/opt/pybin/python/pybin/files/keys/scrapedKeys.txt', 'w+')  as f:
                 pass
 
 #remove line break char
@@ -73,14 +74,14 @@ while True:
             scrapedKeys[index] = value.rstrip("\n")
         #print(scrapedKeys)#testprint
 
-#check to see if keyList keys already exist in scrapedKeys, if not skip and write to scrapedList      
+#check to see if keyList keys already exist in scrapedKeys, if not skip and write to scrapedList
         for index, value in enumerate(keyList):
             if value in scrapedKeys:
                 print(value)
                 pass
             else :
                 scrapedList.append(keyList[index])
-            
+
        #print(scrapedList)#print keys to scrape
 
 #Append scrapedKeys File
@@ -98,26 +99,27 @@ while True:
             reqResult = requests.get(rawPasteURL+i)
         #print(reqResult.text)#test print of raw pastes
             pageDict[i] = reqResult.text
-   
+
         #for k,v in pageDict.items():
             #print(k,v)#test print of dict with pastes
- 
+
         #print("             ")#spacer
-        #here you can replace this with regex for what you are looking for in this case the word password is simple for testing and finding interesting stuff.
-        refilter = re.compile(r'(?is)password')
+        #change the bellow regex to match what you are looking for.
+        refilter = re.compile(r'(?ism)password')
+        
 
         for x,y in pageDict.items():
-            result = refilter.findall(y) 
+            result = refilter.findall(y)
     #print(result)
     #print(type(result))
     #print(len(result))
             if len(result) >= 1:
                 sep = "*" * 30
                 with open(os.path.join('/opt/pybin/python/pybin/files/pastes/',"pasteList"+".txt"), 'a+') as f:
-                    f.write("\n{0} Paste Key: {1} Search Result: {2} URL: {3}".format(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),x,result[0], f"https://pastebin.com/{x}"))
-    
+                    f.write("{0} Paste Key: {1} Search Result: {2} URL: {3}\n".format(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),x,result[0], f"https://pastebin.com/{x}"))
+
         time.sleep(1)
-    
+
     except Exception as e:
         filelogger.exception("EXCEPTIONSTART", exc_info=True)
         pass
